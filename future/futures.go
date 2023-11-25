629c4589cab661d28d3b272ad4e2186d68334a7b
hamzagoc <hamzaburakhan@gmail.com>
2023-11-25 20:30:21 +0300

    collect errors

--- future/futures.go
+++ future/futures.go
@@ -16,8 +16,21 @@ func WaitAllSilently[T any](futures []*Future[T]) {
 	wg.Wait()
 }
 
+func WaitAllContinueOnError[T any](futures []*Future[T]) []error {
+	return waitAll(futures, false)
+}
+
 func WaitAll[T any](futures []*Future[T]) error {
+	errors := waitAll(futures, true)
+	if len(errors) > 0 {
+		return errors[0]
+	}
+	return nil
+}
+
+func waitAll[T any](futures []*Future[T], abortOnError bool) []error {
 	ch := make(chan *Future[T], len(futures))
+	errs := make([]error, 0, len(futures))
 
 	for i := range futures {
 		go func(future *Future[T]) {
@@ -26,14 +39,16 @@ func WaitAll[T any](futures []*Future[T]) error {
 		}(futures[i])
 	}
 
-	// Wait for all. If any error occurred, don't wait and return first error.
 	for i := 0; i < len(futures); i++ {
 		ft := <-ch
 		if ft.Err != nil {
-			return ft.Err
+			errs = append(errs, ft.Err)
+			if abortOnError {
+				break
+			}
 		}
 	}
-	return nil
+	return errs
 }
 
 func GetAll[T any](futures []*Future[T]) ([]T, error) {
@@ -52,6 +67,10 @@ func GetAll[T any](futures []*Future[T]) ([]T, error) {
 
 func WaitFor(futures ...*Future[any]) error {
 	return WaitAll(futures)
+}
+
+func WaitForContinueOnError(futures ...*Future[any]) []error {
+	return WaitAllContinueOnError(futures)
 }
 
 func GetResult[T any](f *Future[any]) T {
